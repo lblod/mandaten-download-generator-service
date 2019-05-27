dcc7fb81f75f2031043f7ff6c7b727f8f40b2e5d
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-05-27 15:41:57 +0200

    clean up spaces

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ new CronJob(cronFrequency, function() {
 
 /**
  * Triggers an async export task for the mandatendatabank and writes the data dumps to files in /data/exports
- * 
+ *
  * @return [202] if export started successfully. Location header contains an endpoint to monitor the task status
  * @return [503] if an export task is already running
 */
@@ -28,7 +28,7 @@ app.post('/export-tasks', async function(req, res, next) {
     const task = await insertNewTask();
 
     task.perform(); // don't await this call since the export is executed asynchronously
-  
+
     return res.status(202).location(`/export-tasks/${task.id}`).end();
   } catch(e) {
     return next(new Error(e.message));
@@ -37,7 +37,7 @@ app.post('/export-tasks', async function(req, res, next) {
 
 /**
  * Get the status of a task
- * 
+ *
  * @return [200] with task status object
  * @return [404] if task with given id cannot be found
 */
