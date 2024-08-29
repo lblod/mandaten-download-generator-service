0e6162d88f1a9a743f4a9a30177cae12d6c348a4
benjay10 <ben.behaeghe@redpencil.io>
2024-08-29 14:36:31 +0200

    All variables to env file

--- env.js
+++ env.js
@@ -1,5 +1,7 @@
 import envvar from 'env-var';
 
+// Environment variables
+
 export const EXPORT_CRON_PATTERN = envvar
   .get('EXPORT_CRON_PATTERN')
   .required()
@@ -46,5 +48,77 @@ export const EXPORT_FILE_BASE = envvar
 export const FILE_OUTPUT_DIR = envvar
   .get('FILE_OUTPUT_DIR')
   .required()
-  .default('/data/exports')
+  .default('/share/exports')
   .asString();
+
+export const JOBS_GRAPH = envvar
+  .get('JOBS_GRAPH')
+  .required()
+  .default('http://mu.semte.ch/graphs/system/jobs')
+  .asUrlString();
+
+export const FILES_GRAPH = envvar
+  .get('FILES_GRAPH')
+  .required()
+  .default('http://mu.semte.ch/graphs/system/jobs')
+  .asUrlString();
+
+export const EXPORT_CLASSIFICATION_URI = envvar
+  .get('EXPORT_CLASSIFICATION_URI')
+  .required()
+  .default('http://redpencil.data.gift/id/exports/concept/GenericExport')
+  .asUrlString();
+
+export const TASK_OPERATION_URI = envvar
+  .get('TASK_OPERATION_URI')
+  .required()
+  .asUrlString();
+
+export const CSV_EXPORT_SPARQL_FILE = envvar
+  .get('CSV_EXPORT_SPARQL_FILE ')
+  .default('/config/csv-export.sparql')
+  .asString();
+
+// Constants
+
+export const STATUS_BUSY =
+  'http://redpencil.data.gift/id/concept/JobStatus/busy';
+export const STATUS_SCHEDULED =
+  'http://redpencil.data.gift/id/concept/JobStatus/scheduled';
+export const STATUS_SUCCESS =
+  'http://redpencil.data.gift/id/concept/JobStatus/success';
+export const STATUS_FAILED =
+  'http://redpencil.data.gift/id/concept/JobStatus/failed';
+export const STATUS_CANCELED =
+  'http://redpencil.data.gift/id/concept/JobStatus/canceled';
+
+export const JOB_OPERATION_URI =
+  'http://redpencil.data.gift/id/jobs/concept/JobOperation/DownloadGeneration';
+export const JOB_TYPE = 'http://vocab.deri.ie/cogs#Job';
+export const TASK_TYPE = 'http://redpencil.data.gift/vocabularies/tasks/Task';
+export const EXPORT_TYPE =
+  'http://redpencil.data.gift/vocabularies/exports/Export';
+export const ERROR_TYPE = 'http://open-services.net/ns/core#Error';
+
+export const PREFIXES = `
+  PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+  PREFIX task: <http://redpencil.data.gift/vocabularies/tasks/>
+  PREFIX dct: <http://purl.org/dc/terms/>
+  PREFIX prov: <http://www.w3.org/ns/prov#>
+  PREFIX nie: <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#>
+  PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+  PREFIX oslc: <http://open-services.net/ns/core#>
+  PREFIX cogs: <http://vocab.deri.ie/cogs#>
+  PREFIX adms: <http://www.w3.org/ns/adms#>
+  PREFIX export: <http://redpencil.data.gift/vocabularies/exports/>
+  PREFIX nfo: <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#>
+  PREFIX dbpedia: <http://dbpedia.org/ontology/>
+`;
+
+export const TASK_URI_PREFIX = 'http://redpencil.data.gift/id/task/';
+export const JOB_URI_PREFIX = 'http://redpencil.data.gift/id/job/';
+export const CONTAINER_URI_PREFIX =
+  'http://redpencil.data.gift/id/dataContainers/';
+export const ERROR_URI_PREFIX = 'http://redpencil.data.gift/id/jobs/error/';
+export const JOB_CREATOR_URI =
+  'http://lblod.data.gift/services/DownloadGeneratorService';
